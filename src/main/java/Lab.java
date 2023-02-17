d806e462dadfb5bbae440bf347fd1b249bcc2b03
Taite Jernigan <taite5100@gmail.com>
2023-02-17 23:59:20 +0000

    Completed Lab While Loop

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -35,9 +35,10 @@ public class Lab {
     public int whileExample(int end){
         int sum = 0;
         int i = 0;
-
-//        code here
-
+        while (i < end) {
+            sum += i;
+            i++;
+        }
         return sum;
     }
 }
